921d78eecc6de39a6ae407e9b4a034b1a97dd209
natmacira <natimciraolo@gmail.com>
2022-04-11 09:18:33 -0300

    Correción de espacios en el código después de lo viste en clase

--- js/script.js
+++ js/script.js
@@ -1,36 +1,18 @@
 window.addEventListener("load", function() {
-
+    //Burger Menu
     const burgerButton = document.getElementById('burger-button');
     const itemsNav     = document.getElementsByClassName('items-list-nav');
-
-    const burgerToggler = () => {
-        document.body.classList.toggle("burger-menu-opened");
-
-    }
-
-     const closedBurger = () => {
-         document.body.classList.remove("burger-menu-opened");
-     }
-
+    const burgerToggler = () =>  document.body.classList.toggle("burger-menu-opened");
+    const closedBurger = () => document.body.classList.remove("burger-menu-opened");
     burgerButton.addEventListener('click', burgerToggler);
-
     for (let i = 0; i < itemsNav.length; i++) {
-        itemsNav[i].addEventListener('click', closedBurger);
-        
-    }
-     
-
-
-   
+        itemsNav[i].addEventListener('click', closedBurger);       
+    }      
+    //Modal 
     const enterModalBtn = document.getElementById('ingresar-modal-btn');
     const closedModalBtn = document.getElementById('close-modal');
-
-    const modalToggler = () => {
-
-        document.body.classList.toggle("modal-button-opened");
-    }
-
-    enterModalBtn.addEventListener('click', modalToggler);
+    const modalToggler = () =>  document.body.classList.toggle("modal-button-opened");
+        enterModalBtn.addEventListener('click', modalToggler);
     closedModalBtn.addEventListener('click', modalToggler);
     
 
